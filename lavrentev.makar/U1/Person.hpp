a295b25b2d73b5c6d48ca56cf321e09f65aa1336
lavrentev.makar <makar.lavrentev@yandex.ru>
2026-06-25 10:55:54 +0300

    Added struct and started to write main

---
+++ lavrentev.makar/U1/Person.hpp
@@ -0,0 +1,8 @@
+#include <cstddef>
+#include <string>
+
+struct Person
+{
+  size_t id;
+  std::string info;
+};
---
+++ lavrentev.makar/U1/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+  if (argc != 2 && argc != 0)
+  {
+    std::cerr << "Input processing error" << "\n";
+    return 1;
+  }
+
+  
+}
